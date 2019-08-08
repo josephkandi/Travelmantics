c334f0044f35aa382b575017fdbcc6c798504ebb
Joseph Kandi <joseph@apps4u.co.za>
2019-08-08 16:02:55 +0200

    Initial commit

---
+++ app/src/main/java/edu/self/josephkandi/travelmantics/activity/AdminActivity.java
@@ -0,0 +1,119 @@
+package edu.self.josephkandi.travelmantics.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+
+import edu.self.josephkandi.travelmantics.R;
+
+public class AdminActivity extends AppCompatActivity implements OnCompleteListener<UploadTask.TaskSnapshot> {
+
+    private static final int REQUEST_CODE = 200;
+    private static final String TAG = AdminActivity.class.getSimpleName();
+    TextInputEditText textInputEditTextPlace;
+    TextInputEditText textInputEditTextAmount;
+    TextInputEditText textInputEditTextDescription;
+    ImageView imageViewPlace;
+    Uri placeImageUri;
+
+    FirebaseFirestore firestore;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+        firestore = FirebaseFirestore.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        textInputEditTextPlace = findViewById(R.id.tiePlace);
+        textInputEditTextAmount = findViewById(R.id.tieAmount);
+        textInputEditTextDescription = findViewById(R.id.tieDescription);
+        imageViewPlace = findViewById(R.id.imgPlace);
+    }
+
+    public void pickImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{ "image/jpeg", "image/png"});
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            placeImageUri = data.getData();
+            imageViewPlace.setImageURI(placeImageUri);
+            storageReference = firebaseStorage.getReference("images");
+            storageReference.putFile(placeImageUri).addOnCompleteListener(this);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.admin,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_save:
+                saveDeal();
+                break;
+        }
+        return true;
+    }
+
+    private void saveDeal() {
+        HashMap<String, Object> deal = new HashMap<>();
+        deal.put("place", textInputEditTextPlace.getText().toString());
+        deal.put("amount", textInputEditTextAmount.getText().toString());
+        deal.put("description", textInputEditTextDescription.getText().toString());
+        deal.put("placeImageUri", placeImageUri.toString());
+
+        firestore.collection("deals").add(deal);
+    }
+
+
+
+    @Override
+    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+        Log.d(TAG, "Complete");
+        if(task.isSuccessful()){
+            storageReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    placeImageUri = task.getResult();
+                }
+
+            });
+        } else {
+            Toast.makeText (AdminActivity.this, "Unable to save image", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/edu/self/josephkandi/travelmantics/activity/UserActivity.java
@@ -0,0 +1,16 @@
+package edu.self.josephkandi.travelmantics.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.self.josephkandi.travelmantics.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
